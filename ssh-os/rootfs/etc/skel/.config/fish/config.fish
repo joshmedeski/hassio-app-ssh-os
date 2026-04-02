521e407f0b43012b3a847cdb9b0024fc89c87201
Josh Medeski <josh.medeski@gmail.com>
2026-04-02 14:36:21 -0500

    feat: add XDG environment variables to fish config

--- ssh-os/rootfs/etc/skel/.config/fish/config.fish
+++ ssh-os/rootfs/etc/skel/.config/fish/config.fish
@@ -1,3 +1,8 @@
+# XDG directories (persist config in /data, separate from /homeassistant)
+set -gx XDG_CONFIG_HOME /data/.config
+set -gx XDG_DATA_HOME /data/.local/share
+set -gx XDG_STATE_HOME /data/.local/state
+
 starship init fish | source
 
 if status is-interactive; and not set -q TMUX
